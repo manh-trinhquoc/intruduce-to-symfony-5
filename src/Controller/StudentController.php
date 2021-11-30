649d04742ad3ba5458dec774feb7e021ab00a814
manhtq <manhtq@sunshinegroup.vn>
2021-11-30 10:24:41 +0700

    sp

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -13,9 +13,9 @@ class StudentController extends AbstractController
     /**
      * @Route("/student", name="student_list")
      */
-    public function list()
+    public function listAction()
     {
-        $studentRepository = new StudentRepository();
+        $studentRepository = $this->getDoctrine()->getRepository('App:Student');
         $students = $studentRepository->findAll();
 
         $template = 'student/list.html.twig';
@@ -30,16 +30,15 @@ class StudentController extends AbstractController
     /**
      * @Route("/student/{id}", name="student_show")
      */
-    public function show($id)
+    public function show(Student $student)
     {
-        $studentRepository = new StudentRepository();
-        $student = $studentRepository->findOne($id);
+        $studentRepository = $this->getDoctrine()->getRepository('App:Student');
         $template = 'student/show.html.twig';
         if (!$student) {
             $template = 'error/404.html.twig';
         }
         $args = [
-        'student' => $student
+            'student' => $student
         ];
         return $this->render($template, $args);
     }
@@ -52,5 +51,49 @@ class StudentController extends AbstractController
         $student = new Student();
         $student->setFirstName($firstName);
         $student->setSurname($surname);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($student);
+        $em->flush();
+        return $this->redirectToRoute('student_show', [
+            'id' => $student->getId()
+        ]);
+        return new Response('Created new student with id '.$student->getId());
+    }
+
+    /**
+    * @Route("/student/delete/{id}")
+    */
+    public function delete(Student $student)
+    {
+        // entity manager
+        $em = $this->getDoctrine()->getManager();
+        $studentRepository = $this->getDoctrine()->getRepository('App:Student');
+        // find thge student with this ID
+        $id = $student->getId();
+        // tells Doctrine you want to (eventually) delete the Student (no queries yet)
+        $em->remove($student);
+        // actually executes the queries (i.e. the DELETE query)
+        $em->flush();
+        return new Response('Deleted student with id '.$id);
+    }
+
+    /**
+    * @Route("/student/update/{id}/{newFirstName}/{newSurname}")
+    */
+    public function update(Student $student, $newFirstName, $newSurname)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $id = $student->getId();
+        if (!$student) {
+            throw $this->createNotFoundException(
+                'No student found for id '.$id
+            );
+        }
+        $student->setFirstName($newFirstName);
+        $student->setSurname($newSurname);
+        $em->flush();
+        return $this->redirectToRoute('student_show', [
+            'id' => $id
+        ]);
     }
 }
